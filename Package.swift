1c3c5a72911ad48e925ed417e09c87dc11c2d2bc
sylvia-tencent-wmt <150782251+sylvia-tencent-wmt@users.noreply.github.com>
2023-11-28 17:39:38 +0800

    Initial commit

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
             targets: ["VKIDSDK"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/VKCOM/VKSDK-iOS.git", .upToNextMajor(from: "0.89.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
